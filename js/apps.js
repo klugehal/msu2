e8f2e8a8939850a50261e351df6d061221dbc537
Haley Kluge <haleykluge@Haleys-MacBook-Pro.local>
2017-01-28 17:20:39 -0500

    graphfinish

--- js/apps.js
+++ js/apps.js
@@ -1,4 +1,4 @@
-var margin = { top: 10, right: 13, bottom: 50, left: 50}
+var margin = { top: 10, right: 13, bottom: 60, left: 50}
 
 var width = 425 - margin.left - margin.right;
 var height = 625 -margin.top - margin.bottom;
@@ -10,11 +10,13 @@ var svg = d3.select('.chart')
 	.append('g')
 		.attr ('transform', `translate(${margin.left}, ${margin.top})`);
 
-svg.append('rect')
-	.attr('width', width)
-	.attr('height', height)
-	.style('fill', 'lightblue')
-	.style('stroke', 'green')
+var data = [
+	{ score:63, subject: 'Basketball'},
+	{ score:98, subject: 'Football'},
+	{ score:30, subject: 'Hockey'},
+	{ score:14, subject: 'Tennis'},
+	{ score:26, subject: 'Volleyball'}
+];
 
 var yScale = d3.scaleLinear()
 	.domain([0,100])
@@ -25,16 +27,30 @@ var yAxis = d3.axisLeft(yScale).ticks(5);
 
 svg.call(yAxis)
 
-var xScale = d3.scaleTime()
-	.domain([new Date(2016,0,1), new Date(2017,0,1)])
+var xScale = d3.scaleBand()
+	.paddingInner(0.1)
+	.paddingOuter(0.4)
+	.domain(data.map(d => d.subject))
 	.range([0,width]);
 
 var xAxis = d3.axisBottom(xScale)
 	.ticks(5)
-	.tickSizeOuter(5)
-	.tickPadding(7);
+	.tickSizeOuter(15)
+	.tickPadding(5);
 
 svg
 	.append('g')
 		.attr('transform',`translate(0, ${height})`)
-	.call(xAxis);
+	.call(xAxis)
+		.selectAll('text')
+		.style('text-anchor', 'start')
+		.attr('transform','rotate(45)');
+
+svg.selectAll('rect')
+	.data(data)
+	.enter()
+	.append('rect')
+	.attr('x', d => xScale(d.subject))
+	.attr('y', d => yScale(d.score))
+	.attr('width', d => xScale.bandwidth())
+	.attr('height', d => height - yScale(d.score));
