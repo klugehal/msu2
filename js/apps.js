258dd32a7c8ff5ebb7e67be496b8023a56635b79
Haley Kluge <haleykluge@Haleys-MacBook-Pro.local>
2017-01-28 15:02:56 -0500

    quantizescale

--- js/apps.js
+++ js/apps.js
@@ -1,8 +1,9 @@
-var timeScale = d3.scaleTime()
-	.domain([new Date (2016,0,1), new Date()])
-	.range ([0,100]);
+var quantizeScale = d3.scaleQuantize()
+	.domain([0,100])
+	.range(['red','green','white']);
 
-	console.log(timeScale(new Date(2016, 9, 10)));
-	console.log(timeScale(new Date(2016, 7, 29)));
+	console.log(quantizeScale(30));
+	console.log(quantizeScale(60));
+	console.log(quantizeScale(90));
 
-	console.log(timeScale.invert(50));
+	console.log(quantizeScale.invertExtent('white'));
