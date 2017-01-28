e2a031cc7f847648a4b273ef665ef4d1a0c1927a
Haley Kluge <haleykluge@Haleys-MacBook-Pro.local>
2017-01-28 16:04:31 -0500

    update data

--- js/apps.js
+++ js/apps.js
@@ -1,7 +1,21 @@
-var ordinalScale = d3.scaleOrdinal()
-	.domain(['cold','cool','okay','nice','warm'])
-	.range(['blue','light blue','white','yellow','green']);
+d3.json("data.json", function(data){
+	
+	var min = d3.min(data, function(d){
+		return d.age;
+	});
 
-console.log(ordinalScale('cool'));
-console.log(ordinalScale('warm'));
-console.log(ordinalScale('okay'));
+	console.log(min);
+
+	var max = d3.max(data, function(d){
+		return d.age;
+	});
+
+	var ages = d3.set(data, function(d){
+		return d.age;
+	})
+
+	console.log(ages.values());
+
+})
+
+
